9ebaa4becc675e2d3e8a470dc6f7a3e4a5f8b5c0
tirthankar27 <tirthankarghosh4@gmail.com>
2026-07-06 23:08:02 +0530

    Updated

--- terraform/security.tf
+++ terraform/security.tf
@@ -62,6 +62,23 @@ resource "azurerm_network_security_rule" "https" {
   network_security_group_name = azurerm_network_security_group.retailiq.name
 }
 
+resource "azurerm_network_security_rule" "nodeport" {
+  name                        = "Allow-NodePort"
+  priority                    = 140
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+
+  source_port_range           = "*"
+  destination_port_range      = "30080"
+
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+
+  resource_group_name         = azurerm_resource_group.retailiq.name
+  network_security_group_name = azurerm_network_security_group.retailiq.name
+}
+
 # Jenkins
 resource "azurerm_network_security_rule" "jenkins" {
   name                        = "Allow-Jenkins"
